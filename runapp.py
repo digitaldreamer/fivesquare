a284ae134bb3dca83321a3c48268de360fbf93be
digitaldreamer <poyzer@gmail.com>
2015-08-05 17:27:59 -0400

    path error

--- runapp.py
+++ runapp.py
@@ -5,5 +5,5 @@ from waitress import serve
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    app = loadapp('config:production.ini', relative_to='.')
+    app = loadapp('config:production.ini', relative_to='./service')
     serve(app, host='0.0.0.0', port=port)
