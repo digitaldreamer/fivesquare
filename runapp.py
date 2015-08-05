0de4104b5d2c21b1c5908a0127b498b7aa62d9bd
digitaldreamer <poyzer@gmail.com>
2015-08-05 18:32:00 -0400

    try changing directories to initialize the app

--- runapp.py
+++ runapp.py
@@ -3,10 +3,12 @@ import os
 from paste.deploy import loadapp
 from waitress import serve
 
+
 if __name__ == "__main__":
     cwd = os.getcwd()
-    os.chdir(os.path.sep.join([cwd, 'service']))
+    # os.chdir(os.path.sep.join([cwd, 'service']))
+    path = os.path.sep.join([cwd, 'service'])
 
     port = int(os.environ.get("PORT", 5000))
-    app = loadapp('config:production.ini', relative_to='.')
+    app = loadapp('config:production.ini', relative_to=path)
     serve(app, host='0.0.0.0', port=port)
