67c069af80f233c57d6f2a3b22bb653231f5865b
digitaldreamer <poyzer@gmail.com>
2015-08-05 18:01:39 -0400

    cwd before starting the app

--- runapp.py
+++ runapp.py
@@ -4,6 +4,9 @@ from paste.deploy import loadapp
 from waitress import serve
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    app = loadapp('config:production.ini', relative_to='./service')
+    cwd = os.getcwd()
+    os.chdir(os.path.sep.join([cwd, 'service']))
+
+    port = int(os.environ.get("PORT", 5000))
+    app = loadapp('config:production.ini', relative_to='.')
     serve(app, host='0.0.0.0', port=port)
